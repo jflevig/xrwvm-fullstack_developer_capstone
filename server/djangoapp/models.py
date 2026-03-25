c0943e2341cd806f90a21468c1718994df343ff9
jflevig <jfelevi@gmail.com>
2026-03-25 12:40:39 -0300

    python files linted

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -14,7 +14,7 @@ class CarMake(models.Model):
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -23,12 +23,13 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2026,
+    year = models.IntegerField(
+        default=2026,
         validators=[
             MaxValueValidator(2026),
             MinValueValidator(2015)
-        ])
+        ]
+    )
 
     def __str__(self):
         return self.name  # Return the name as the string representation
-
